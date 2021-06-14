ec08a27157256fc0549e9ac43a620faabe706e86
Mykyta_Meshcheryakov <nik.meshcheryakov@gmail.com>
2021-06-14 22:29:31 +0300

    Dictionaries

---
+++ JetBrains/Dictionaries.py
@@ -0,0 +1,15 @@
+sample = {}
+sample['a'] = 3
+sample['b'] = 5
+sample['c'] = -2
+print(sample['a'] + sample['b'] + sample['c'])
+print(sample['a'])
+print(sample['b'])
+print(sample['c'])
+print(sample)
+
+shopping_list = {'bananas': 5, 'oranges': 3, 'yogurt': 2, 'chicken breasts': 3, 'olive oil': 1}
+shopping_list['bananas'] = 10
+print(shopping_list)
+
+
